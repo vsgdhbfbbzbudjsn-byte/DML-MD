feefc18f720982f716f9aa62556ff5a5209c70e5
DML <daudymussa1705@gmail.com>
2025-07-12 20:53:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "DML-MD~SMEzzD6B#-S_hYaOCNI1QuPhj3Gbs9B79pHN61Uoc2zRjwkSM8DE",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
