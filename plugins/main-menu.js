6f6dcb24b8e0a97e14527cb5dea3054178aec625
DML <daudymussa1705@gmail.com>
2025-07-23 21:20:18 +0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -5,7 +5,7 @@ const {runtime} = require('../lib/functions')
 const axios = require('axios')
 
 cmd({
-    pattern: "menu2",
+    pattern: "menu",
     alias: ["allmenu","fullmenu"],
     use: '.menu2',
     desc: "Show all bot commands",
