6b086e09fa36356b37c67b58492d91e64cb019f8
DML <daudymussa1705@gmail.com>
2025-07-30 19:03:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -59,7 +59,7 @@ ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words  
 MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
-ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
+ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "true",
 // make anti link true,false for groups 
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
