4c5ab081e2ff521ae3ac9e18e15dca6c5ddda61a
DML <daudymussa1705@gmail.com>
2025-08-14 18:41:03 +0300

    Update config.js

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "255622220680",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "255615752312",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "DML",
 // add bot owner name
