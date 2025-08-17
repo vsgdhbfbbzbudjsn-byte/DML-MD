2ae6b17d0d26f2df77269c26d9d69a7fb16d6143
DML <daudymussa1705@gmail.com>
2025-08-17 10:56:24 +0300

    Update yt-seaech.js

--- plugins/yt-seaech.js
+++ plugins/yt-seaech.js
@@ -6,10 +6,11 @@ const ytdl = require('yt-search');
 const fs = require('fs-extra')
 var videotime = 60000 // 1000 min
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
+
 cmd({
     pattern: "yts",
     alias: ["ytsearch"],
-    use: '.yts dml',
+    use: '.yts Dml',
     react: "🔎",
     desc: "Search and get details from youtube.",
     category: "search",
@@ -19,21 +20,34 @@ cmd({
 
 async(conn, mek, m,{from, l, quoted, body, isCmd, umarmd, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if (!q) return reply('*Please give me words to search*')
-try {
-let yts = require("yt-search")
-var arama = await yts(q);
-} catch(e) {
+  if (!q) return reply('*Please give me words to search*')
+  try {
+    let yts = require("yt-search")
+    var arama = await yts(q);
+  } catch(e) {
     l(e)
-return await conn.sendMessage(from , { text: '*Error !!*' }, { quoted: mek } )
-}
-var mesaj = '';
-arama.all.map((video) => {
-mesaj += ' *🖲️' + video.title + '*\n🔗 ' + video.url + '\n\n'
-});
-await conn.sendMessage(from , { text:  mesaj }, { quoted: mek } )
+    return await conn.sendMessage(from , { text: '*Error !!*' }, { quoted: mek } )
+  }
+  var mesaj = '';
+  arama.all.map((video) => {
+    mesaj += ' *🖲️' + video.title + '*\n🔗 ' + video.url + '\n\n'
+  });
+
+  await conn.sendMessage(from , {
+    text: mesaj,
+    contextInfo: {
+      forwardingScore: 999,
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: "120363387497418815@newsletter",
+        newsletterName: "DML-YTS",
+        serverMessageId: 1
+      }
+    }
+  }, { quoted: mek })
+
 } catch (e) {
-    l(e)
+  l(e)
   reply('*Error !!*')
 }
 });
