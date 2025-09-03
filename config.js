d56c0ab853c0b31a7676f969bdf8fa00866ad23a
vsgdhbfbbzbudjsn-byte <vsgdhbfbbzbudjsn@gmail.com>
2025-09-03 20:48:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "DML-MD~trdxDBqI#fVCG2E5EBMMAu1APN-qIPR32-r4BvPXLJqhsV8TEElw",
+SESSION_ID: process.env.SESSION_ID || "DML-MD~9iNwgayD#BGxN3pIrFFfvAKIvkCEuaKGRTz7NEflNOFzE-jxVhlQ",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
